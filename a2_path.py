7b9f93fe5434ea8542566e2d0000b2ba9c782129
Esther Ejidike <ejidikeesther@gmail.com>
2025-10-12 04:59:34 +0100

    Start Task 2

---
+++ a2_path.py
@@ -0,0 +1,31 @@
+"""
+Hinger Project
+Coursework 001 for: CMP-7058A Artificial Intelligence
+
+Includes functions for retrieving safe paths for Task 2
+
+@author: E6 (100501127)
+@date: 29/09/2025
+
+"""
+
+def path_BFS(start,end):
+    return
+
+def path_DFS(start,end):
+    return
+
+def path_IDDFS(start, end):
+    return
+
+def path_astar(start, end):
+    return
+
+def compare():
+    return
+
+def tester():
+    return
+
+if __name__ == '__main__':
+    tester()
